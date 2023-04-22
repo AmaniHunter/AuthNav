338e4eef3b4278112dd0f113065a2bd3544822ad
Amani Hunter <amanihunter@AmanisHcBookPro.attlocal.net>
2023-04-22 18:39:22 -0400

    initial commit

---
+++ AuthNav/AuthNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthNavApp.swift
+//  AuthNav
+//
+//  Created by Amani Hunter on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct AuthNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
